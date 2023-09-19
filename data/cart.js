17cea072bf474a053d1d11e8c73aefec1eb6e843
projectzero00 <Contatoproject03@gmail.com>
2023-09-19 06:46:50 -0300

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,16 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) 
+|| [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 1,
 }, {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1
-}];
+}];;
+
+
+function saveCart() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export let addToCart = (productId) => {
     let matchingProduct
@@ -23,7 +29,7 @@ export let addToCart = (productId) => {
             quantity: select
         }
         cart.push(newProduct);
-
+        saveCart();
     }
 }
 
@@ -35,4 +41,5 @@ export const removeFromCart = (productId) => {
         }
     })
     cart = newCart;
+    saveCart();
 }
